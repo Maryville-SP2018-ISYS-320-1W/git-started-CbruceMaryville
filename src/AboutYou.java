164376f8afcaed0476af404b92be9f59cee4e841
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-03-18 21:28:51 -0500

    Update AboutYou.java

--- src/AboutYou.java
+++ src/AboutYou.java
@@ -4,10 +4,10 @@ public class AboutYou {
 	public static void main(String[] args) {
 		// TODO Modify the values in the String literals in ALL_CAPS below
 		System.out.print("Hello, my name is ");
-		System.out.println("YOUR_NAME");
+		System.out.println("Clayton Bruce");
 
 		System.out.print("And I'm taking this class because ");
-		System.out.println("BECAUSE_WHY");
+		System.out.println("Becaue it is required to complete my Cyber Security major here at Maryville!");
 	}
 
 }
